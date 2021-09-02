7fa8c03ce9a7ce5783d13cd5c1fef8b3cf19cc64
DungHoang <kathymaidung@gmail.com>
2021-09-02 10:01:47 +0300

    add image with Plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 export default function index() {
   return (
     <main>
       <Layout pageTitle="Home Page">
         <p>I'm making this by following the Gatby Tutorial.</p>
+        <StaticImage
+          alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+          src="../images/dog.jfif"
+        />
       </Layout>
     </main>
   );
